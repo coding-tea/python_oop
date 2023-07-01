f09d80ccb0196d70e09298277fc85d8be9aa4f8b
digital opium <test@ismail.com>
2023-07-01 17:14:51 +0100

    matricule and nom getters and setters

--- conseiller.py
+++ conseiller.py
@@ -8,3 +8,19 @@ class conseiller:
         self._codecomplexe = codecomplexe
         self._nombreVisites = nombreVisites
         self._codeSecret = codeSecret
+
+    @property
+    def matricule(self):
+        return self.matricule
+    
+    @matricule.setter
+    def matricule(self, newMatricule):
+        self.matricule = newMatricule
+
+    @property
+    def nom(self):
+        return self.nom
+    
+    @nom.setter
+    def nom(self, newNom):
+        self.nom = newNom
