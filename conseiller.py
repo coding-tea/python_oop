61b8b5d470117203abacc29b7119a7080bd80dbc
digital opium <test@ismail.com>
2023-07-01 17:09:06 +0100

    create the constucter

--- conseiller.py
+++ conseiller.py
@@ -2,4 +2,9 @@
 
 class conseiller:
     def __init__(self, matricule, nom, prenom, codecomplexe, nombreVisites, codeSecret):
-        pass
+        self._matricule = matricule
+        self._nom = nom
+        self._prenom = prenom
+        self._codecomplexe = codecomplexe
+        self._nombreVisites = nombreVisites
+        self._codeSecret = codeSecret
